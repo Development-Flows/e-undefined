665fb155574345eca57421b375e091ea46636be4
Furkan <furkanktnci@gmail.com>
2023-04-08 17:43:05 +0300

    admin/panel updated.

--- pages/admin/panel/index.tsx
+++ pages/admin/panel/index.tsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import { Menu } from 'antd';
+import { useState } from 'react';
+import "../../../styles/reset.css";
+import Link from 'next/link';
 
-const Panel = () => {
-  return (
-    <div>Panel</div>
-  )
+function getItem(label:React.ReactNode,  key?: React.Key | null, children?: MenuItem[],  theme?: any, ) {
+  return {
+    key,
+    children,
+    label,
+    theme,
+  };
 }
 
-export default Panel
+const Panel = () => {
+  const [current, setCurrent] = useState();
+  const onClick = (e: any) => {
+  setCurrent(e.key);
+  };
+  const items:MenuItem[] = [
+    getItem('Anasayfa', '1'),
+    getItem('Ürünler', 'sub1', [
+    getItem(<Link href={"/admin/product"}>Ürünler</Link>, '3'),
+    ]),
+    getItem('Kategori Yönetimi', '5' ),
+   
+  ];
+  return (
+      <Menu
+        onClick={onClick}
+        style={{
+          width: 232,
+          height: "100vh"
+        }}
+        defaultSelectedKeys={['1']}
+        mode="inline"
+        theme="dark"
+        items={items}
+        getPopupContainer={(node) => node.parentNode}
+      />
+  );
+};
+export default Panel;
