a377b0afcda9fb3064e768c96e8d2f1dfe3cab07
Kerim Can Gökdağ <53795573+gokdag@users.noreply.github.com>
2023-04-15 00:05:23 +0300

    store provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,21 +5,23 @@ import Footer from "../components/Footer/";
 // General CSS Files
 import "../styles/global.css";
 import "../styles/reset.css";
-
-
+import {StoreProvider} from "@/store/store-provider";
 // FontFamily
-import { Inter } from "@next/font/google";
-const interFontFamily = Inter({ subsets: ["latin"] });
+import {Inter} from "@next/font/google";
 
-export default function RootLayout({ children } : {children : React.ReactNode}) {
+const interFontFamily = Inter({subsets: ["latin"]});
+
+export default function RootLayout({children}: { children: React.ReactNode }) {
     return (
-      <html lang="tr" className={interFontFamily.className}>
+        <html lang="tr" className={interFontFamily.className}>
         <body>
-          <Header/>
-          <div className="content">{children}</div>
-          <Footer/>
+        <StoreProvider>
+            <Header/>
+            <div className="content">{children}</div>
+            <Footer/>
+        </StoreProvider>
         </body>
-      </html>
+        </html>
     );
-  }
+}
   
