572cd91cb795374d5b3dd9e399f54f24b7251196
Kerim Can Gökdağ <53795573+gokdag@users.noreply.github.com>
2023-10-31 20:47:47 +0300

    added name prop to inputComp

--- components/InputComp/index.tsx
+++ components/InputComp/index.tsx
@@ -1,34 +1,36 @@
 "use client"
-import React, { useState } from 'react';
-import { Input } from 'antd';
+import React, {useState} from 'react';
+import {Input} from 'antd';
 import styles from './index.module.scss';
 
 type PropsType = {
-  inputText: string;
-  placeHolderTitle: string;
-  onChange?: (value: string) => void
+    inputText: string;
+    placeHolderTitle: string;
+    name: string;
+    onChange?: (name: string, value: string) => void
 };
 
-const InputComp = ({ inputText, placeHolderTitle, onChange }: PropsType) => {
-  const [inputValue, setInputValue] = useState('')
+const InputComp = ({inputText, name, placeHolderTitle, onChange}: PropsType) => {
+    const [inputValue, setInputValue] = useState('')
 
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setInputValue(event.target.value)
-    if (typeof onChange === "function") {
-      onChange(event.target.value)
-    }
-  };
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setInputValue(event.target.value)
+        if (typeof onChange === "function") {
+            onChange(event.target.name, event.target.value)
+        }
+    };
 
-  return (
-    <div className={styles.inputClass}>
-      <p className={styles.inputTitle}>{inputText}</p>
-      <Input
-        placeholder={placeHolderTitle}
-        value={inputValue}
-        onChange={handleInputChange}
-      />
-    </div>
-  );
+    return (
+        <div className={styles.inputClass}>
+            <p className={styles.inputTitle}>{inputText}</p>
+            <Input
+                name={name}
+                placeholder={placeHolderTitle}
+                value={inputValue}
+                onChange={handleInputChange}
+            />
+        </div>
+    );
 };
 
 export default InputComp;
