a744128c85e995ab23fd9b8b3c2bda262519c9e0
Enes Ozturk <enes55ozturk@gmail.com>
2023-11-24 17:59:57 +0300

    Components Updated

--- components/SelectComp/index.tsx
+++ components/SelectComp/index.tsx
@@ -1,38 +1,39 @@
-"use clinet"
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 import { Select, Space } from 'antd';
-import type { SelectProps } from 'antd';
 
 
-const options = [
-    { label: 'Enes', value: 'enes' },
-    { label: 'Enes2', value: 'enes2' },
-    { label: 'Enes3', value: 'enes3' },
-    { label: 'Enes4', value: 'enes4' },
-]
+
+interface OptionsTypes {
+    label: string,
+    value: number | string
+}
 
 type PropsType = {
     selectTitle: string;
+    placeHoler: string;
     onChange?: (value: string) => void;
+    options: OptionsTypes[]
 
 }
-const handleChange = (value: string[]) => {
-    console.log(value)
 
-};
+const SelectComp = ({ selectTitle, onChange, options, placeHoler }: PropsType) => {
+    const [valueData, setValueData] = useState("")
 
-
-const SelectComp = ({ selectTitle, onChange }: PropsType) => {
-
+    const handleChange = (value: string) => {
+        setValueData(value)
+        if (typeof onChange === "function") {
+            onChange(value)
+        }
+    };
     return (
         <div>
             <p>{selectTitle}</p>
             <Space style={{ width: '100%' }} direction="vertical">
                 <Select
-                    mode="multiple"
                     allowClear
                     style={{ width: '100%' }}
-                    placeholder="Please select"
+                    placeholder={placeHoler}
                     onChange={handleChange}
                     options={options}
                 />
